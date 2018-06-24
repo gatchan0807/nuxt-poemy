01d5f4cf6c93475554a28a87cc894197b4001c6e
ahaha0807 <b5331@oic.jp>
2018-06-24 15:59:16 +0900

    I created input form for posting poem.
    markup the this component. styling later.

--- components/common/Post.vue
+++ components/common/Post.vue
@@ -1,13 +1,28 @@
 <template>
   <div>
-    <textarea name="post" id="post" cols="30" rows="10"></textarea>
+    <textarea v-model="value" name="post" id="post" cols="30" rows="10" @keyup="count"></textarea>
+    <span>{{counter}}</span>
     <button @click="submit">詩う</button>
   </div>
 </template>
 
 <script>
   export default {
-    name: "post"
+    name: 'post',
+    data() {
+      return {
+        value: '',
+        counter: 0
+      }
+    },
+    methods: {
+      submit: function () {
+        console.log(this.value)
+      },
+      count: function () {
+        this.counter = this.value.length
+      }
+    }
   }
 </script>
 
