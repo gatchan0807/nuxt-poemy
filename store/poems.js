b94527bbfceb22e5899a902005bac807e08f20a2
ahaha0807 <b5331@oic.jp>
2018-06-25 09:11:27 +0900

    I update it to xhr connect mutation.
    using async/await methods.

--- store/poems.js
+++ store/poems.js
@@ -1,17 +1,32 @@
 import shortId from 'shortid'
 import moment from 'moment'
+import axios from 'axios'
+
+const BASE_URL = 'http://localhost:3030'
 
 export const state = () => ({
-  list: []
+  list: [],
 })
 
 export const mutations = {
-  add(state, poem) {
-    state.list.push({
+  async add(state, poem) {
+    const p = {
       id: shortId.generate(),
       contents: poem.contents,
-      timestamp: moment().format('x')
-    })
+      timestamp: Math.floor(moment().format('x') / 1000)
+    }
+
+    state.list.push(p)
+
+    let data = {}
+
+    try {
+      data = await axios.post(BASE_URL + '/poems', p)
+    } catch (e) {
+      console.log(e)
+    }
+
+    console.log(data)
   }
 }
 
