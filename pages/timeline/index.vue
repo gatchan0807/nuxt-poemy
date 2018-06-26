b0225c0223e7ada0dbbb4408e8e1e9260f04b27a
ahaha0807 <b5331@oic.jp>
2018-06-26 09:02:00 +0900

    [WIP] I create time-line page.
    to load poems from store. bad, "mounted" is necessary for some reason.

---
+++ pages/timeline/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <section class="container">
+    <ul>
+      <li v-for="poem in poems" v-bind:key="poem.id">
+        {{poem.id}} / {{poem.contents}} / {{poem.timestamp}}
+      </li>
+    </ul>
+  </section>
+</template>
+
+<script>
+  import { mapGetters } from 'vuex'
+
+  export default {
+    computed: {
+      ...mapGetters({
+        poems: 'poems/all'
+      })
+    },
+    mounted: function () {
+      console.log(this.poems)
+    },
+    name: 'index',
+    data() {
+    }
+
+  }
+</script>
+
+<style scoped>
+
+</style>
