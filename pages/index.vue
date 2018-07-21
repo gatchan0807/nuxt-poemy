8747e1e7e7621cb0b04c9352caa7631a3c82cb16
ahaha0807 <b5331@oic.jp>
2018-07-21 23:41:22 +0900

    I change the top page to timeline page.

--- pages/index.vue
+++ pages/index.vue
@@ -1,64 +1,45 @@
 <template>
   <section class="container">
-    <div>
-      <app-logo/>
-      <h1 class="title">
-        nuxt-poemy
-      </h1>
-      <h2 class="subtitle">
-        For poemer SNS.
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
-      </div>
-    </div>
+    <ul>
+      <li v-for="poem in poems" v-bind:key="poem.id">
+        <poem :contents="poem.contents" :poemId="poem.id"></poem>
+      </li>
+    </ul>
   </section>
 </template>
 
 <script>
-import AppLogo from '~/components/AppLogo.vue'
+  import { mapActions, mapGetters } from 'vuex'
+  import Poem from '~/components/poem/Poem.vue'
 
-export default {
-  components: {
-    AppLogo
+  export default {
+    components: {
+      Poem
+    },
+    computed: mapGetters({
+      poems: 'poems/all'
+    }),
+    mounted: function () {
+      this.getPoems()
+    },
+    data() {
+      return {}
+    },
+    methods: {
+      ...mapActions({
+        getPoems: 'poems/list'
+      }),
+    },
   }
-}
 </script>
 
 <style>
-.container {
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
+  li {
+    border-bottom: 1px solid #7f828b;
+  }
 
-.title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
+  p {
+    height: 4.5rem;
+    overflow: hidden;
+  }
 </style>
